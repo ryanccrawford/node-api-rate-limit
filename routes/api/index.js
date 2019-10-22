cd406dea5623a19482ad73e591224c6bc84640bd
Ryan Crawford <ryanccrawford@live.com>
2019-10-22 00:10:35 -0400

    api routes added. rate limiter added

--- routes/api/index.js
+++ routes/api/index.js
@@ -6,4 +6,12 @@ router.get('/', function(req, res, next) {
     res.json({ message: "Welcome to the api, please read the read.me file for information on how to use this api." });
 });
 
+router.get('/get/:data', function(req, res, next) {
+
+    let data = req.params.data
+
+    res.json({ message: `you got all data for ${data}` });
+
+});
+
 module.exports = router;
--- server.js
+++ server.js
@@ -1,22 +1,49 @@
-const createError = require('http-errors');
-const express = require('express');
-const bodyParser = require('body-parser')
-const path = require('path');
-const logger = require('morgan');
+/*
+ *   Rate Limited by User IP + Path / Params
+ *   Basic rate-limiting middleware for Express.
+ *   By Nathan Friedly : https://www.npmjs.com/package/express-rate-limit
+ */
+const createError = require("http-errors");
+const express = require("express");
+const bodyParser = require("body-parser");
+const logger = require("morgan");
 
-// The main router for the api endpoints
-const apiRouter = require('./routes/api');
+/*
+ *   NPM Package express-rate-limit
+ *   Basic rate-limiting middleware for Express.
+ *   By Nathan Friedly : https://www.npmjs.com/package/express-rate-limit
+ */
+const rateLimit = require("express-rate-limit");
+
+// The API endpoints
+const apiRouter = require("./routes/api");
 
 //Creates our express app
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Create limiter options
+const limiter = rateLimit({
+    windowMs: 1 * 60 * 1000, // 1 minutes
+    max: 10, // limit each IP to 10 requests per minute
+    statusCode: 401,
+    message: "Access Denied. API rate limit reached. Please wait before sending more request.",
+    keyGenerator: req => {
+        // Combines the users IP address and the request path an returns it as the key
+        let key = req.ip + req.url;
+        return key;
+    }
+});
 
 // Load Middleware
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(bodyParser.json({ type: 'application/*+json' }))
+app.use(bodyParser.json({ type: "application/*+json" }));
 app.use(express.urlencoded({ extended: false }));
+//Load middleware limiter on all routes
+app.use("/", limiter);
 
-// Load Routes
+// Load API Routes
 app.use(apiRouter);
 
 // Catch 404's and forward to error handler
@@ -28,11 +55,15 @@ app.use((req, res, next) => {
 app.use((err, req, res, next) => {
     // set locals, only providing error in development
     res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+    res.locals.error = req.app.get("env") === "development" ? err : {};
 
     // render the error page
     res.status(err.status || 500);
-    res.json({ 'message': err.status });
+    res.json({ message: err.status });
+});
+
+app.listen(PORT, function() {
+    console.log(`API Server now listening on PORT ${PORT}!`);
 });
 
 module.exports = app;
